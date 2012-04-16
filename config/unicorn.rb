33d44455dcbd4eab54a7fc9293c7903e44f7949a
Arun Agrawal <arunagw@Ubuntu-1004-lucid-64-minimal.(none)>
2012-04-16 13:19:49 +0200

    unicorn modified

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,7 +21,7 @@ worker_processes 4
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory "/home/arun/domains/railsconf2012_twitter_leaderboard" # available in 0.94.0+
+working_directory "/home/arunagw/home/arun/domains/railsconf2012_twitter_leaderboard" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
@@ -32,13 +32,13 @@ listen 9292, :tcp_nopush => true
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/home/arun/domains/railsconf2012_twitter_leaderboard/tmp/pids/unicorn.pid"
+pid "/home/arunagw/home/arun/domains/railsconf2012_twitter_leaderboard/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/home/arun/domains/railsconf2012_twitter_leaderboard/log/unicorn.stderr.log"
-stdout_path "/home/arun/domains/railsconf2012_twitter_leaderboard/log/unicorn.stdout.log"
+stderr_path "/home/arunagw/home/arun/domains/railsconf2012_twitter_leaderboard/log/unicorn.stderr.log"
+stdout_path "/home/arunagw/home/arun/domains/railsconf2012_twitter_leaderboard/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
